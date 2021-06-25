fb4a7eb5d5fcce9382a50e67cde1f1b20d45a095
James Rostirolla <j_rostirolla@hotmail.com>
2021-06-25 11:55:13 +1000

    webpage asthetics complete, HTML and CSS updated

---
+++ script.js
@@ -0,0 +1,62 @@
+let questOne = {
+    correct: "correctTester",
+    wrong1: "wrong1",
+    wrong2: "wrong2",
+    wrong3: "wrong3",
+}
+let questTwo = {
+    correct: "correctTester",
+    wrong1: "wrong1",
+    wrong2: "wrong2",
+    wrong3: "wrong3",
+}
+
+let questThree = {
+    correct: "correctTester",
+    wrong1: "wrong1",
+    wrong2: "wrong2",
+    wrong3: "wrong3"
+}
+
+let questFour = {
+    correct: "correctTester",
+    wrong1: "wrong1",
+    wrong2: "wrong2",
+    wrong3: "wrong3",
+}
+
+let questFive = {
+    correct: "correctTester",
+    wrong1: "wrong1",
+    wrong2: "wrong2",
+    wrong3: "wrong3",
+}
+/*
+How it will work: BRAINSTORM
+- Have a basic card that has the information for the desired moment in time
+    - Start with intro screen (see framework for reference)
+    - When start button pressed, the JS file overwrites the text in the card with the desired information
+        - TODO: Basically, one way for this to work, you can have separate cards which are hidden until called (create boxes first, each with own ID. Each ID set to hidden until called)
+        - TODO: OR, you can have the JS rewrite the contents of each card so that it in turn shows the contents you wish to show (WARNING! this options seems more complicated, ABORT!!!)
+        - Have a <h2> with card info ("Welcome", "Question", "Highscore", etc)
+        - Have <p> with the intro info and the instructions, which is collapsed and hidden when the "start" button is pressed
+        - Have buttons, or internal boxes, that are hidden on initial page load, then loaded when start button pressed
+        - 
+*/
+
+
+//TODO: Create timer (stick to 60 seconds)
+    //TODO: Link Start button to timer
+    //TODO: Link timer to answer arrays
+
+//TODO: Create question arrays (lets aim to do 10)
+    //TODO: Create the if statement for if (answer === correct) {textContent: Correct!} else {textContent: Wrong!}
+    //TODO: Create the screen to show the questions
+
+//TODO: Create intro screen
+    //TODO: Create the card for intro screen
+
+//TODO: Create link to the "highscore" screen
+    //TODO: Create highscore screen
+
+//TODO:
